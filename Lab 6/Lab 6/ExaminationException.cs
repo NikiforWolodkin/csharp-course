a17a708b91ed95b3cc92a42a53b65a9272d79bc6
user <mrdoggydog37@gmail.com>
2022-11-04 17:27:30 +0300

    Finished laboratory work 6

---
+++ Lab 6/Lab 6/ExaminationException.cs
@@ -0,0 +1,36 @@
+﻿using Lab_6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class ExaminationArgumentException : ArgumentException
+    {
+        public int IncorrectValue { get; set; }
+        public ExaminationArgumentException(string message, int incorrectValue) : base(message)
+        {
+            IncorrectValue = incorrectValue;
+        }
+    }
+
+    public class QuestionArgumentException : ArgumentException
+    {
+        public int IncorrectValue { get; set; }
+        public QuestionArgumentException(string message, int incorrectValue) : base(message)
+        {
+            IncorrectValue = incorrectValue;
+        }
+    }
+
+    public class SessionException : Exception
+    {
+        public Examination.ExaminationChallenge IncorrectExam { get; set; }
+        public SessionException(string message, Examination.ExaminationChallenge incorrectExam) : base(message)
+        {
+            IncorrectExam = incorrectExam;
+        }
+    }
+}
--- Lab 6/Lab 6/Program.cs
+++ Lab 6/Lab 6/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using Lab_6;
+using static Lab_6.Examination;
+
+Console.WriteLine("Hello World!");
+
+try
+{
+    Exam exam1 = new Exam(-2);
+}
+catch
+{
+    Console.WriteLine("Произошла ошибка!");
+}
+try
+{
+    Exam exam2 = new Exam(20);  
+}
+catch
+{
+    Console.WriteLine("Произошла ошибка!");
+}
+try
+{
+    Exam exam3 = new Exam(3);
+    Session session1 = new Session();
+    session1.RemoveExam(exam3);
+}
+catch
+{
+    Console.WriteLine("Произошла ошибка!");
+}
+try
+{
+    Session session2 = null;
+    var exams = session2.Exams;
+}
+catch 
+{
+    Console.WriteLine("Произошла ошибка!");
+}
+try
+{
+    int vaiable1 = 0;
+    int variable = 5 / vaiable1;
+}
+catch
+{
+    Console.WriteLine("Произошла ошибка!");
+}
+Console.WriteLine();
+
+try
+{
+    try
+    {
+        Exam exam = new Exam(-5);
+    }
+    catch (ExaminationArgumentException ex)
+    {
+        Console.WriteLine("Произошла ошибка!");
+        Console.WriteLine(ex.Message);
+        throw;
+    }
+}
+catch (ExaminationArgumentException ex)
+{
+    Console.WriteLine(ex.IncorrectValue);
+    Console.WriteLine(ex.Source);
+    Console.WriteLine(ex.StackTrace);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Выполнение операции прекращено.");
+    Debug.Fail("Произошла ошибка");
+}
+
