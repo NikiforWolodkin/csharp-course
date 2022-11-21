b79d47d5851f45ddd44d5a94016deff5cafbe37c
User <mrdoggydog37@gmail.com>
2022-11-21 22:46:33 +0300

    Started work on laboratory work 12

---
+++ Lab 12/Lab 12/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lab_12;
+
+VNDLog.Clear();
+
+VNDDiskInfo.GetFreeSpace(@"C:\");
+VNDDiskInfo.GetDriveFormat(@"C:\");
+VNDDiskInfo.GetDriveInfo();
+Console.WriteLine();
+
+VNDFileInfo.GetPath(@"../../../vndlogfile.txt");
+VNDFileInfo.GetFileInfo(@"../../../vndlogfile.txt");
+VNDFileInfo.GetFileTime(@"../../../vndlogfile.txt");
+Console.WriteLine();
+
+VNDDirInfo.GetFileCount(@"../../../../Lab 12");
+VNDDirInfo.GetDirectoryCount(@"../../../../Lab 12");
+VNDDirInfo.GetDirectoryTime(@"../../../../Lab 12");
+VNDDirInfo.GetRoot(@"../../../../Lab 12");
+Console.WriteLine();
+
+VNDLog.ClearOldLogs();
+
+VNDLog.Read("GetRoot");
+Console.WriteLine();
+VNDLog.Read(DateTime.Today);
+Console.WriteLine();
