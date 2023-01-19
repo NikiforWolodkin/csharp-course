c6a752319c5645bb3e20a28e9da7c4382c36358b
User <mrdoggydog37@gmail.com>
2023-01-19 18:54:40 +0300

    Added task with 2DPoint

---
+++ Exam/2DPoint/2DPoint/2DPoint.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DPoint
+{
+    public class DeleteException : Exception
+    {
+        public Point Point;
+
+        public DeleteException(Point point, string message) : base(message)
+        {
+            Point = point;
+        }
+    }
+
+    public class Publisher
+    {
+        public EventHandler<EventArgs> Change;
+
+        public void RaiseChange() => OnChange(EventArgs.Empty);
+
+        protected virtual void OnChange(EventArgs e)
+        {
+            EventHandler<EventArgs> raiseEvent = Change;
+
+            if (raiseEvent != null)
+            {
+                raiseEvent(this, e);
+            }
+        }
+    }
+
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point point)
+            {
+                if (point.X == X && point.Y == Y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class Path
+    {
+        public List<Point> Points { get; set; }
+
+        public Path()
+        {
+            Points = new List<Point>();
+        }
+
+        public Path(params Point[] points)
+        {
+            Points = new List<Point>(points);
+        }
+
+        public void Add(Point point) => Points.Add(point);
+        public void Clear() => Points.Clear();
+
+        public void Delete(Point point)
+        {
+            if (!Points.Contains(point))
+            {
+                throw new DeleteException(point, "Point is not a part of the path");
+            }
+
+            Points.Remove(point);
+        }
+
+        public void Subscribe(Publisher publisher)
+        {
+            publisher.Change += handleChange;
+        }
+
+        public void handleChange(object sender, EventArgs e)
+        {
+            foreach (var point in Points)
+            {
+                point.X = -point.X;
+                point.Y = -point.Y;
+            }
+        }
+
+        public (int, int, int, int) CountPoints()
+        {
+            int pointsPerQuater = Points.Count / 4;
+
+            switch (Points.Count % 4)
+            {
+                case 0:
+                    return (pointsPerQuater, pointsPerQuater, pointsPerQuater, pointsPerQuater);
+                    break;
+                case 1:
+                    return (pointsPerQuater + 1, pointsPerQuater, pointsPerQuater, pointsPerQuater);
+                    break;
+                case 2:
+                    return (pointsPerQuater + 1, pointsPerQuater + 1, pointsPerQuater, pointsPerQuater);
+                    break;
+                case 3:
+                    return (pointsPerQuater + 1, pointsPerQuater + 1, pointsPerQuater + 1, pointsPerQuater);
+                    break;
+                case 4:
+                    return (pointsPerQuater + 1, pointsPerQuater + 1, pointsPerQuater + 1, pointsPerQuater + 1);
+                    break;
+                default:
+                    throw new Exception("Couldn't count points;");
+                    break;
+            }
+        }
+
+        public static bool operator ==(Path path1, Path path2)
+        {
+            return path1.Points.SequenceEqual(path2.Points);
+        }
+
+        public static bool operator !=(Path path1, Path path2)
+        {
+            return !path1.Points.SequenceEqual(path2.Points);
+        }
+    }
+}
---
+++ Exam/2DPoint/2DPoint/Program.cs
@@ -0,0 +1,59 @@
+﻿using _2DPoint;
+
+_2DPoint.Path path = new _2DPoint.Path(new Point(4, 3), new Point(8, 3), new Point(6, 3));
+
+Point point = new Point(7, -5);
+
+path.Add(point);
+Console.WriteLine(path.Points.Count);
+
+path.Delete(point);
+Console.WriteLine(path.Points.Count);
+
+path.Clear();
+Console.WriteLine(path.Points.Count);
+
+_2DPoint.Path path2 = new _2DPoint.Path(new Point(4, 3), new Point(8, 3), new Point(6, 3), point);
+
+Console.WriteLine(path == path);
+Console.WriteLine(path == path2);
+
+(int, int, int, int) points = path2.CountPoints();
+Console.WriteLine($"{points.Item1}, {points.Item2}, {points.Item3}, {points.Item4}");
+
+try
+{
+    path.Delete(point);
+}
+catch (DeleteException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Publisher publisher = new Publisher();
+path2.Subscribe(publisher);
+
+foreach (var pt in path2.Points)
+{
+    Console.WriteLine($"{pt.X}, {pt.Y}");
+}
+publisher.RaiseChange();
+foreach (var pt in path2.Points)
+{
+    Console.WriteLine($"{pt.X}, {pt.Y}");
+}
+
+Type type = path.GetType();
+
+foreach (var constructor in type.GetConstructors())
+{
+    Console.WriteLine(constructor);
+}
+foreach (var field in type.GetFields())
+{
+    Console.WriteLine(field);
+}
+foreach (var property in type.GetProperties())
+{
+    Console.WriteLine(property);
+}
