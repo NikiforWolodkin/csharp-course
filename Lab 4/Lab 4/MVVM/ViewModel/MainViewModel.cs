1af76b3497fe145eb62b98a2e52b66527642ad5c
User <mrdoggydog37@gmail.com>
2023-03-07 13:51:21 +0300

    Added new item

---
+++ Lab 4/Lab 4/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Lab_4.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.MVVM.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public ProductViewModel ProductVM { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public MainViewModel()
+        {
+            ProductVM = new ProductViewModel();
+            CurrentView = ProductVM;
+        }
+    }
+}
